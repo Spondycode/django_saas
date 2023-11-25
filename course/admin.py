707c83d294d33065e0b82db4d2a9377d217e65be
David Allen <spondicious@me.com>
2023-11-25 21:30:27 +0100

    courses/views.py

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at', 'instructor']
+    search_fields = ['title', 'instructor__username']
+    list_filter = ['created_at', 'updated_at', 'instructor']
+    
